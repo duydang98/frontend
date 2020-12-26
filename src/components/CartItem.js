1d1534ca6758f2fb34ac1c088a5d2283681851de
Your Name <you@example.com>
2020-12-26 09:19:37 +0700

    project

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -10,15 +10,25 @@ function CartItem(props) {
     const removeCartItem = (id_product) =>{
            dispatch(removeFromCart(id_product));
     }
- 
+    let area = "TP Hồ Chí Minh";
+    if(item.id_area===2){
+        area = "TP Cần Thơ";
+    }
+    if(item.id_area===3){
+        area = "Đà Lạt";
+    }
+
     return (
-        <div className="row">
+        <div className="row cart-it">
             <div className="col-xs-2"><img className="img-responsive img-cart" src={item.image} alt={item.id_product} />
             </div>
-            <div className="col-xs-4">
-                <h4 className="product-name"><strong>{item.name_product}</strong></h4><h4><small>{item.unit_product}</small></h4>
+            <div className="col-xs-3">
+                <h4 className="product-name"><strong>{item.name_product}</strong></h4><h4><small>Unit: {item.unit_product}</small></h4>
             </div>
-            <div className="col-xs-6">
+            <div className="col-xs-3">
+                <h4 className="product-name"><strong>{area}</strong></h4><h4><small>Area Stock</small></h4>
+            </div>
+            <div className="col-xs-4 cart-it-1">
                 <div className="col-xs-6 text-right">
                     <h6><strong> {item.price_product} <span className="text-muted"> x</span></strong></h6>
                 </div>
@@ -27,7 +37,7 @@ function CartItem(props) {
                     
                     onChange={(e) =>
                         dispatch(
-                          addToCart(item.id_product,item.id_area,Number(e.target.value))
+                          addToCart(item.id_product,item.id_area,parseInt(e.target.value))
                         )
                       }
                     />
